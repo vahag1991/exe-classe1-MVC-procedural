6a1b4aa5994689a2a81521784a85a23c981b14a1
vahag1991 <smbatyanvahagn1991@gmail.com>
2025-06-16 11:03:18 +0200

    exe

--- controller/PrivateController.php
+++ controller/PrivateController.php
@@ -1,2 +1,28 @@
 <?php
 # controller/PrivateController.php
+
+if(isset($_GET['pageChanger'])){
+    // on veut se déconnecter
+    if($_GET['pageChanger']==="disconnect") {
+        $disconnect = disconnectUser();
+        if ($disconnect) {
+            header("Location: ./");
+            exit();
+        }
+    // on veut atteindre l'admin
+    }elseif ($_GET['pageChanger']==="admin"){
+
+        // si on tente d'insérer un article
+        if(isset($_POST['article_title'])){
+            $insert = addArticle($db,$_POST,$_SESSION['iduser']);
+            if($insert){
+                header("Location: ./?message=Article bien inséré");
+                exit();
+            }
+            $error = "Article non inséré <a href='#' onclick='history.go(-1)'>retour au formulaire</a>";
+        }
+
+        // appel de la vue
+        require_once "../view/admin.html.php";
+    }
+}
